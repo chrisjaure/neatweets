fe574fb99146371800eb89ba30139534c746f972
Chris Jaure <chrisjaure@gmail.com>
2011-10-23 20:56:27 -0500

    make the web server functional

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 var Http = require('http'),
 	Fs = require('fs'),
-	Tweetpic = require('./twitterpic');
+	Tweetpic = require('./twitterpic'),
+	Connect = require('connect');
 
-getLocalTweets();
+Connect(
+	Connect.vhost('cleverchris.com', Connect(
+		Connect.router(routes)
+	)),
+	Connect.router(routes)
+).listen(8000);
+
+function routes (app) {
+	app.get('/neatweets/:username', function(req, res){
+		getTweets(req.params.username, function(err, tweets){
+			if (err) {
+				res.statusCode = 500;
+				res.end(err.message || err);
+				return;
+			}
+
+			var canvas = Tweetpic.generate(tweets);
+
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'image/png');
+			res.end(canvas.toBuffer(), 'utf-8');
+		});
+	});
+}
 
 function getLocalTweets() {
 	Fs.readFile('./tweets.json', 'utf8', function(err, data){
@@ -17,10 +41,10 @@ function getLocalTweets() {
 	});
 }
 
-function getTweets() {
+function getTweets(username, callback) {
 	Http.get({
 		host:'api.twitter.com',
-		path: '/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=chrisjaure&count=200'
+		path: '/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name='+ username +'&count=190'
 	}, function(res){
 		var body = '';
 		res.on('data', function(chunk){
@@ -28,13 +52,33 @@ function getTweets() {
 			body += chunk.toString('utf8');
 
 		}).on('end', function(){
-			var tweets = JSON.parse(body);
+			var tweets;
 
-			writeImage(tweets);
+			try {
+				tweets = JSON.parse(body);
+			}
+			catch(e) {
+				if (typeof callback == 'function') {
+					callback(e);
+				}
+				return;
+			}
+
+			if (typeof callback == 'function') {
+				if (tweets.error) {
+					callback(tweets.error);
+				}
+				else {
+					callback(null, tweets);				
+				}
+
+			}
 		});
 
-	}).on('error', function(){
-		console.log('error');
+	}).on('error', function(err){
+		if (typeof callback == 'function') {
+			callback(err);
+		}
 	});
 }
 
